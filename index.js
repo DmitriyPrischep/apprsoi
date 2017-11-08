f3ed96968ebcd13fc1c0680fb447e8db699b5a04
pdvdev <pdvdev@yandex.ru>
2017-11-08 12:10:04 +0300

    create app

---
+++ index.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var port = 3000;
+
+function onRequest(request, response) {
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello world!\nIt's my first simple server on heroku");
+	response.end();
+}
+
+http.createServer(onRequest).listen(port, function (err) {
+	if (err) {
+		return console.log('Something bad happened', err);
+	}
+	console.log('Server is listening on ' + port);
+});
